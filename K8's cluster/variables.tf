67e2e7b8e1120d03a45cbf39e257c58f2cdecca1
jmcgowan11 <jmcgowan@kinly.com>
2022-10-27 10:59:38 +0100

    v1.4

--- K8's cluster/variables.tf
+++ K8's cluster/variables.tf
@@ -11,5 +11,9 @@ variable "cluster" {
 }
 
 variable "kubernetes_ver" {
-  default = "latest"
+  default = "1.21"
+}
+
+variable "token"{
+  default = "a9c13830ac135581da220b46ebc5245452df756f46c54c15bdd59516254bfd0c"
 }
